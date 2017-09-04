fd7d3a9c90da4f7bba2ad189b503e082c8f03046
David Ramirez <dramirez2096@gmail.com>
2017-09-03 17:10:52 -0700

    console.log names added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 function showForm(){
   document.querySelector('.hidden').style.display="inline";
 }
+
+var firstName = document.querySelector('#firstName');
+var lastName = document.querySelector('#lastName');
+var userName = document.querySelector('#userName');
+
+document.querySelector('form.hidden').addEventListener('submit', function (e) {
+
+    e.preventDefault();
+
+    console.log(firstName.value);
+    console.log(lastName.value);
+    console.log(userName.value);
+});
